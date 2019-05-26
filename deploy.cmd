2ab647b6f81edd7fd2a6f38891e53f00059ff9c2
Jez Reel Maghuyop <jezreelmaghuyop@gmail.com>
2019-05-27 01:48:20 +0800

    update kudu deployment command

--- deploy.cmd
+++ deploy.cmd
@@ -85,9 +85,6 @@ goto :EOF
 :: Deployment
 :: ----------
 
-:: Deployment
-:: ----------
-
 :Deployment
 echo Handling node.js deployment.
 
@@ -97,13 +94,13 @@ call :SelectNodeVersion
 :: 2. Install npm packages
 IF EXIST "%DEPLOYMENT_SOURCE%\package.json" (
   pushd "%DEPLOYMENT_SOURCE%"
-  call :ExecuteCmd !NPM_CMD! install --production
+  call :ExecuteCmd !NPM_CMD! install
   IF !ERRORLEVEL! NEQ 0 goto error
   popd
 )
 
-:: 3. Angular Prod Build
-IF EXIST "%DEPLOYMENT_SOURCE%\angular.json" (
+:: 3. Angular Prod Build //If you had generated this yourself then please add this step manually!!)
+IF EXIST "%DEPLOYMENT_SOURCE%/angular.json" (
 echo Building App in %DEPLOYMENT_SOURCE%…
 pushd "%DEPLOYMENT_SOURCE%"
 call :ExecuteCmd !NPM_CMD! run build
@@ -115,7 +112,7 @@ popd
 
 :: 4. KuduSync
 IF /I "%IN_PLACE_DEPLOYMENT%" NEQ "1" (
-  call :ExecuteCmd "%KUDU_SYNC_CMD%" -v 50 -f "%DEPLOYMENT_SOURCE%/dist" -t "%DEPLOYMENT_TARGET%" -n "%NEXT_MANIFEST_PATH%" -p "%PREVIOUS_MANIFEST_PATH%" -i ".git;.hg;.deployment;deploy.cmd"
+  call :ExecuteCmd "%KUDU_SYNC_CMD%" -v 50 -f "%DEPLOYMENT_SOURCE%/dist/" -t "%DEPLOYMENT_TARGET%" -n "%NEXT_MANIFEST_PATH%" -p "%PREVIOUS_MANIFEST_PATH%" -i ".git;.hg;.deployment;deploy.cmd"
   IF !ERRORLEVEL! NEQ 0 goto error
 )
 
